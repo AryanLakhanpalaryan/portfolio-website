1f6476add84af3b8833e401baf594ebb9e4873e9
AryanLakhanpalaryan <148888913+AryanLakhanpalaryan@users.noreply.github.com>
2024-08-16 00:16:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,170 +1,40 @@
-
-// Dynamic Greeting Based on Time of Day
+// Typewriter effect for greeting
 const hour = new Date().getHours();
 const greeting = document.querySelector('.greeting');
+let greetingText = '';
+
 if (hour < 12) {
-    greeting.textContent = 'Good Morning, I\'m Aryan Lakhanpal';
+    greetingText = 'Good Morning, I\'m Aryan Lakhanpal';
 } else if (hour < 18) {
-    greeting.textContent = 'Good Afternoon, I\'m Aryan Lakhanpal';
+    greetingText = 'Good Afternoon, I\'m Aryan Lakhanpal';
 } else {
-    greeting.textContent = 'Good Evening, I\'m Aryan Lakhanpal';
+    greetingText = 'Good Evening, I\'m Aryan Lakhanpal';
 }
 
-// Particle Background Effect
-const canvas = document.getElementById('particle-canvas');
-const ctx = canvas.getContext('2d');
-let particlesArray;
+greeting.textContent = greetingText;
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-
-let mouse = {
-    x: null,
-    y: null,
-    radius: (canvas.height / 80) * (canvas.width / 80)
-};
-
-window.addEventListener('mousemove', function(event) {
-    mouse.x = event.x;
-    mouse.y = event.y;
-});
-
-class Particle {
-    constructor(x, y, directionX, directionY, size, color) {
-        this.x = x;
-        this.y = y;
-        this.directionX = directionX;
-        this.directionY = directionY;
-        this.size = size;
-        this.color = color;
-    }
-    draw() {
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-        ctx.fillStyle = '#ffffff';
-        ctx.fill();
-    }
-    update() {
-        if (this.x > canvas.width || this.x < 0) {
-            this.directionX = -this.directionX;
+// Initialize Chart.js
+const ctx = document.getElementById('skills-chart').getContext('2d');
+const skillsChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['HTML & CSS', 'JavaScript', 'Java', 'C++', 'Database Management'],
+        datasets: [{
+            label: 'Skill Level',
+            data: [95, 90, 85, 80, 75],
+            backgroundColor: '#17a2b8',
+            borderColor: '#117a8b',
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true,
+                ticks: {
+                    callback: function(value) { return value + '%' }
+                }
+            }
         }
-        if (this.y > canvas.height || this.y < 0) {
-            this.directionY = -this.directionY;
-        }
-        this.x += this.directionX;
-        this.y += this.directionY;
-        this.draw();
     }
-}
-
-function init() {
-    particlesArray = [];
-    let numberOfParticles = (canvas.height * canvas.width) / 9000;
-    for (let i = 0; i < numberOfParticles; i++) {
-        let size = (Math.random() * 5) + 1;
-        let x = (Math.random() * ((innerWidth - size * 2) - (size * 2)) + size * 2);
-        let y = (Math.random() * ((innerHeight - size * 2) - (size * 2)) + size * 2);
-        let directionX = (Math.random() * 5) - 2.5;
-        let directionY = (Math.random() * 5) - 2.5;
-        let color = '#ffffff';
-
-        particlesArray.push(new Particle(x, y, directionX, directionY, size, color));
-    }
-}
-
-function animate() {
-    requestAnimationFrame(animate);
-    ctx.clearRect(0, 0, innerWidth, innerHeight);
-
-    for (let i = 0; i < particlesArray.length; i++) {
-        particlesArray[i].update();
-    }
-}
-
-window.addEventListener('resize', function() {
-    canvas.width = innerWidth;
-    canvas.height = innerHeight;
-    mouse.radius = ((canvas.height / 80) * (canvas.width / 80));
-    init();
 });
-
-window.addEventListener('mouseout', function() {
-    mouse.x = undefined;
-    mouse.y = undefined;
-});
-
-init();
-animate();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
